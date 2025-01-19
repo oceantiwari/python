4e062559bbe1170876564030de99e44069695d41
SAGAR TIWARI <159561158+oceantiwari@users.noreply.github.com>
2025-01-19 23:46:45 +0530

    Add files via upload

--- Python/funcctions/7.py
+++ Python/funcctions/7.py
@@ -1,7 +1,19 @@
 
-def sum(l):
-  for i in l:
-    add=0
-    add+=add+l[i]
-    return add
-print(sum([1,2,3,4,5,6,7,8,9]))
+# def sum(l):
+#   for i in l:
+#     add=0
+#     add=add+l[i]
+#     return add
+# print(sum([1,2,3,4,5,6,7,8,9]))
+
+
+def sum_all(*args):
+  sum=0
+  for i in args:
+    sum+=i
+  return sum
+
+
+print(sum_all(1,2,3,4,5))
+
+
---
+++ Python/funcctions/9.py
@@ -0,0 +1,6 @@
+def even_generator(limit):
+  for i in range(1,limit):
+    if(i%2==0):
+      print (i)
+
+even_generator(30)
