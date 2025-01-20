02e5a859d8e77230c2b2f3140769656a9bc3cdf9
SAGAR TIWARI <159561158+oceantiwari@users.noreply.github.com>
2025-01-20 10:09:05 +0530

    Add files via upload

---
+++ Python/oops/1.py
@@ -0,0 +1,11 @@
+class car:
+  def __init__(self,brand,model):
+    self.brand=brand
+    self.model=model
+my_car=car("tata","safari")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car= car("mahindra","thar")
+print(my_new_car.brand)
+print(my_new_car.model)
